c5e54768e32d66d61d093b3691306c069dc8c9ac
trueaddict <50746660+trueaddict@users.noreply.github.com>
2022-03-23 16:22:24 +0200

    auto gen readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Edariryhmän osallistujat
+Kokoukseen osallistujat:
+
+Virtanen Otto<br>Schoultz Ida-Louise<br>Saarela Venla<br>Kohvakka Jyri este, tilalla Heikkilä Antero<br>Rauhala Nelli<br>
+
+Muut varat:
+
+Hyytiäinen Ossi<br>Saksman Miikael<br>
--- app.py
+++ app.py
@@ -37,6 +37,8 @@ def main():
     """
     )
 
+    md = 'Kokoukseen osallistujat:\n\n'
+
     osallistujat = {}
 
     for i in range(5):
@@ -44,11 +46,13 @@ def main():
         if temp['paikalla'] == 'True':
             osallistujat[temp['Nimi']] = ''
             print(temp['Nimi'])
+            md = md + str(temp['Nimi']) + '<br>'
         else:
             for j in range(5, len(newlist)):
                 temp2 = newlist[j]
                 if temp2['Nimi'] not in osallistujat.keys() and temp2['paikalla'] == 'True':
                     print(temp['Nimi'], 'este, tilalla', temp2['Nimi'])
+                    md = md + str(temp['Nimi']) + ' este, tilalla ' + str(temp2['Nimi']) + '<br>'
                     osallistujat[temp2['Nimi']] = ''
                     break
 
@@ -59,14 +63,19 @@ def main():
     """
     )
 
+    md = md  + '\n\n' + 'Muut varat:\n\n'
+
     for i in newlist:
         if i['Nimi'] not in osallistujat.keys() and i['paikalla'] == 'True':
             print(i['Nimi'])
+            md = md + str(i['Nimi']) + '<br>'
 
     writer = csv.DictWriter(open('jasenet.csv', 'w'), fieldnames=newlist[0].keys())
     writer.writeheader()
     writer.writerows(newlist)
 
+    with open('README.md', 'w') as mdFile:
+        mdFile.write(md)
 
 if __name__ == '__main__':
     main()
